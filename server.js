e4e7e0e3bc942dd541bf8aabdd9e05899b4f659f
paframar <paframar@gmail.com>
2024-02-06 19:18:23 +0100

    deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const router = jsonServer.router('cities.json');
 
 app.use(cors());
-app.use('/api', router);
+app.use('/', router);
 
 const PORT = 9000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
